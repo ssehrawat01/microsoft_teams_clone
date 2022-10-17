926617a3a00ee93e259de20265e4cd1d4dd37ac0
Shubhangi Sehrawat <90876253+ssehrawat01@users.noreply.github.com>
2022-10-16 19:51:50 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID='29fc5a1e-75ce-43a5-a9a3-0835dc304c2c'
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    );
+}
+
+export default App;
